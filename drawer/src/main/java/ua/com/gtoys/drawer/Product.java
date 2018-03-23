8ceb42a23eb43527052cf3de8a68c451071fb1be
Иван Кляповский <klyapovskiy@grandtoys.ukr>
2018-03-23 17:03:51 +0200

    Initial commit

---
+++ drawer/src/main/java/ua/com/gtoys/drawer/Product.java
@@ -0,0 +1,17 @@
+package ua.com.gtoys.drawer;
+
+
+public class Product {
+
+    String title;
+    String desc;
+    String imgSrc;
+
+
+    Product(String _title, String _desc, String _imgSrc) {
+        title = _title;
+        desc = _desc;
+        imgSrc = _imgSrc;
+    }
+
+}
---
+++ drawer/src/main/java/ua/com/gtoys/drawer/ProductShortAdapter.java
@@ -0,0 +1,97 @@
+package ua.com.gtoys.drawer;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+
+
+
+public class ProductShortAdapter extends BaseAdapter {
+
+    Context ctx;
+    LayoutInflater lInflater;
+    ArrayList<Product> objects;
+
+    ProductShortAdapter(Context context, ArrayList<Product> products) {
+        ctx = context;
+        objects = products;
+        lInflater = (LayoutInflater) ctx
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return objects.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return objects.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // используем созданные, но не используемые view
+        View view = convertView;
+        if (view == null) {
+            view = lInflater.inflate(R.layout.product_short, parent, false);
+        }
+
+        Product p = getProduct(position);
+
+        // заполняем View в пункте списка данными из товаров: наименование, цена
+        // и картинка
+        ((TextView) view.findViewById(R.id.textTitleProduct)).setText(p.title);
+        ((TextView) view.findViewById(R.id.textDescProduct)).setText(p.desc);
+        new DownloadImageTask((ImageView) view.findViewById(R.id.imageProduct))
+                .execute(p.imgSrc);
+
+        return view;
+    }
+
+    Product getProduct(int position) {
+        return ((Product) getItem(position));
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error loading img", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
+    }
+
+}
